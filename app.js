82516279d3351d4245e2037023dee75171221152
Youcef <youcefboutrig92@gmail.com>
2021-06-04 14:46:05 +0200

    remove the examples

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.LOCAL_URI;
+const uri = process.env.DB_URI;
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -22,10 +22,8 @@ connection.once('open', () => {
   console.log('MongoDB database connection established successfully');
 });
 
-const exercisesRouter = require('./routes/exercies');
 const usersRouter = require('./routes/users');
 
-app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
 
 app.listen(port, () => {
