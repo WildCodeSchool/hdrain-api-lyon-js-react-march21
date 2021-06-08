7536edaa8ef6265240afa168c478967ae4aca14b
Edouard Misset <edouardmisset@gmail.com>
2021-06-08 16:12:25 +0200

    created functions to launch and stop the copy. Still need to tweak the command to accept username / password

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 const shell = require('shelljs');
 
+// Shell command to copy the file server's content to our server
+const command =
+  'rsync -vrzh wild@51.178.56.162:/home/wild/LOG/2021/05/20/18h50 ~/Downloads';
+
+let intervalId = null;
+
+// Function to execute the shell command to copy the data to our server
 const copyData = () => {
   const date = new Date();
   console.log(date);
-  shell.exec('colorls');
+  shell.exec(command);
 };
 
+// Function to start the periodic copy of files
 const copyDataPeriodically = () => {
   copyData();
-  setInterval(() => {
+  intervalId = setInterval(() => {
     copyData();
   }, 1 * 60 * 1000);
+  console.log(`Data copy interval was started with ID: ${intervalId}`);
+};
+
+// Function to stop the periodic copy of files
+const stopCopyingData = () => {
+  clearInterval(intervalId);
+  console.log(`Interval ${intervalId} was stopped`);
 };
 
 copyDataPeriodically();
+
+module.exports = {
+  copyDataPeriodically,
+  copyData,
+  stopCopyingData,
+};
