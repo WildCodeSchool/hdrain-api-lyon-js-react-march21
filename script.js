11a530e87a052a11a57cdcd742856334865c3c04
Edouard Misset <edouardmisset@gmail.com>
2021-06-09 15:55:10 +0200

    Working script :)

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 const shell = require('shelljs');
+require('dotenv').config();
 
-const localLocationList = ['/Abidjan', '/Toulouse', '/Antibe'];
-const serverAddress = ['51.178.56.162', '...', '...'];
+const destinationPathList = process.env.HD_RAIN_LOCATION_LIST.split(',');
+const serverAddressList = process.env.HD_RAIN_SERVER_LIST.split(',');
 
-// Shell command to copy the file server's content to our server (using ssh)
-const command =
-  ' rsync -vrzh --rsh=ssh wild@51.178.56.162:/home/wild/LOG/2021/05/20/18h50 ~/Downloads';
+// Returns a shell command to copy the file server's content to our server (using rsync through ssh)
+const command = (path, address) =>
+  ` rsync -vrzh --rsh='sshpass -p ${process.env.HD_RAIN_SERVER_PWD} ssh' wild@${address}:/home/wild/LOG/2021/05/20/18h50 ~/Downloads/${path}`;
 
 // Interval in milliseconds
 const interval = 5 * 60 * 1000;
 let intervalId = null;
 
 // Function to execute the shell command to copy the data to our server
-const copyData = () => {
+const copyData = (cmd) => {
   console.log(new Date());
-  shell.exec(command);
+  shell.exec(cmd);
 };
 
 // Function to start the periodic copy of files every 5 minutes
 const copyDataPeriodically = () => {
-  copyData();
   intervalId = setInterval(() => {
-    copyData();
+    destinationPathList.forEach((path, index) => {
+      copyData(command(path, serverAddressList[index]));
+    });
   }, interval);
   console.log(`Data copy interval was started with ID: ${intervalId}`);
 };
@@ -39,3 +41,5 @@ module.exports = {
   copyData,
   stopCopyingData,
 };
+
+// apt-get install sshpass
