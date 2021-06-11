8ea3e9183792a4f0e23047fadc0f525efbcef6d4
Edouard Misset <edouardmisset@gmail.com>
2021-06-11 14:09:03 +0200

    Removed unused files

--- prisma/migrations/20210527085128_create_users_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `user` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `email` VARCHAR(255) NOT NULL,
-    `hashedPassword` VARCHAR(255) NOT NULL,
-
-    UNIQUE INDEX `user.email_unique`(`email`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
