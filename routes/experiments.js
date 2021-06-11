3c174393d2139590aef2a1ebf79fb3b39c0850ea
Edouard Misset <edouardmisset@gmail.com>
2021-06-11 14:11:25 +0200

    Added try catch block

--- routes/experiments.js
+++ routes/experiments.js
@@ -2,16 +2,18 @@ const experimentsRouter = require('express').Router();
 const ExperimentModel = require('../models/ExperimentModel');
 
 experimentsRouter.post('/', async (req, res) => {
-  const { timestamp, log, diagnosticGraph, costGraph } = req.body;
-
-  const newExperiment = await ExperimentModel.create({
-    timestamp,
-    log,
-    diagnosticGraph,
-    costGraph,
-  });
-
-  return res.status(201).send({ newExperiment });
+  try {
+    const { timestamp, log, diagnosticGraph, costGraph } = req.body;
+    const newExperiment = await ExperimentModel.create({
+      timestamp,
+      log,
+      diagnosticGraph,
+      costGraph,
+    });
+    res.status(201).send({ newExperiment });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 module.exports = experimentsRouter;
