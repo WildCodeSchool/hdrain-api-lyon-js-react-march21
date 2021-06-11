bdaaf6809179c275ab8aae0bc695871d3a578d64
Edouard Misset <edouardmisset@gmail.com>
2021-06-11 14:11:25 +0200

    Added try catch block

--- routes/sensors.js
+++ routes/sensors.js
@@ -2,15 +2,17 @@ const sensorsRouter = require('express').Router();
 const SensorModel = require('../models/SensorModel');
 
 sensorsRouter.post('/', async (req, res) => {
-  const { sensorNumber, coord, status } = req.body;
-
-  const newSensor = await SensorModel.create({
-    sensorNumber,
-    coord,
-    status,
-  });
-
-  return res.status(201).send({ newSensor });
+  try {
+    const { sensorNumber, coord, status } = req.body;
+    const newSensor = await SensorModel.create({
+      sensorNumber,
+      coord,
+      status,
+    });
+    res.status(201).send({ newSensor });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 module.exports = sensorsRouter;
