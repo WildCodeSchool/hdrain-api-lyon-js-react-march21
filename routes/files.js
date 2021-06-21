e02f4f040eb49b4b1ef56055f5255c2e32818463
Heranca <heranca.kamalo@gmail.com>
2021-06-21 15:53:29 +0200

    change on db.js

---
+++ routes/files.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const multer  = require('multer');
+
+const filesRouter = require('express').Router();
+
+const upload = multer({ dest: './files/images' });
+
+
+filesRouter.get('/', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+filesRouter.post('/images', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
