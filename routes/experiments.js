0919f35e732ffdb9f279554bfef16b22c23c3d9d
Doriane <berdalado@gmail.com>
2021-06-23 11:49:20 +0200

    experiment route done

--- routes/experiments.js
+++ routes/experiments.js
@@ -1,6 +1,31 @@
 const experimentsRouter = require('express').Router();
 const ExperimentModel = require('../models/ExperimentModel');
 
+experimentsRouter.get('/', async (req, res) => {
+  try {
+    // Retrieve all experiments from the DB
+    const experiments = await ExperimentModel.findMany();
+    if (!experiments.length) res.status(404).send('No experiment found');
+    // Send the result
+    res.status(200).send(experiments);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+experimentsRouter.get('/:id', async (req, res) => {
+  try {
+    const experimentsId = req.params.id;
+    // Retrieve specific sensor from the DB
+    const experiment = await ExperimentModel.findOne(experimentsId);
+    if (!experiment) return res.status(404).send('No experiment found');
+    // Send the result
+    return res.status(200).send(experiment);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+});
+
 experimentsRouter.post('/', async (req, res) => {
   try {
     const {
