b66c4c8783fec6121f2a4fa28bfc55e103e1089c
Edouard Misset <edouardmisset@gmail.com>
2021-07-04 19:59:25 +0200

    some work

--- scripts/readFile.js
+++ scripts/readFile.js
@@ -1,21 +1,26 @@
 const fs = require('fs');
 const util = require('util');
+// const path = require('path');
 
 // Convert `fs.readFile()` into a function that takes the
 // same parameters but returns a promise.
 const readFile = util.promisify(fs.readFile);
 
-const readArrayFromFile = async (path) => {
+const readArrayFromFile = async (pathToFile) => {
   try {
-    // You can now use `readFile()` with `await`!
-    const buffer = await readFile(path);
-    const table = buffer.toString('utf8').trim().split('\n').map(Number);
-    console.log(table);
+    const buffer = await readFile(pathToFile);
+    return buffer.toString('utf8').trim().split('\n').map(Number);
   } catch (error) {
     console.error(error);
+    return [];
   }
 };
 
-readArrayFromFile('./scripts/Jb');
+const test = async () => {
+  const J = await readArrayFromFile('./storage/data/18h45/J');
+  const Jb = await readArrayFromFile('./storage/data/18h45/Jb');
+  console.log(J.length, Jb.length);
+};
+test();
 
 module.exports = readArrayFromFile;
