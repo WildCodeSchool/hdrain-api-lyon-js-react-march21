0e5fa6e0c805a50d8b58cdc4cd2ebf8bac54eca0
Edouard Misset <edouardmisset@gmail.com>
2021-07-05 16:24:23 +0200

    reanamed script in copyScript and added source and destination env variables

--- scripts/copyScript.js
+++ scripts/copyScript.js
@@ -4,9 +4,12 @@ require('dotenv').config();
 const destinationPathList = process.env.HD_RAIN_LOCATION_LIST.split(',');
 const serverAddressList = process.env.HD_RAIN_SERVER_LIST.split(',');
 
+const target = process.env.LOCAL_TARGET;
+const source = process.env.HD_RAIN_SOURCE;
+
 // Returns a shell command to copy the file server's content to our server (using rsync through ssh)
 const command = (path, address) =>
-  ` rsync -vrzh --rsh='sshpass -p ${process.env.HD_RAIN_SERVER_PWD} ssh' wild@${address}:/home/wild/LOG/2021/05/20/18h50 ~/Downloads/${path}`;
+  ` rsync -vrzh --rsh='sshpass -p ${process.env.HD_RAIN_SERVER_PWD} ssh' wild@${address}:${source} ${target}${path}`;
 
 // Interval in milliseconds
 const interval = 5 * 60 * 1000;
