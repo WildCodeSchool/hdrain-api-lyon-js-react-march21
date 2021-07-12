9b257d7c73ba54b9ade40cdc98aafb62c678b59a
Heranca <heranca.kamalo@gmail.com>
2021-07-12 14:33:37 +0200

    merge w/ dev and add findUnique on status model

--- models/StatusModel.js
+++ models/StatusModel.js
@@ -5,4 +5,13 @@ prisma.status.createMany({
     data: newStatusList,
   });
 
-module.exports = {createManyStatus};
+  const findUnique = async (sensorId, experimentId) => {
+    const [status] = await prisma.status.findMany({
+      where: {
+        sensorId: parseInt(sensorId, 10),
+        experimentId: parseInt(experimentId, 10),
+      },
+    });
+    return status;
+  };
+module.exports = {createManyStatus, findUnique};
