84ab6af4ea0edaea425c2b6277973d3060143469
Edouard Misset <edouardmisset@gmail.com>
2021-07-13 16:23:53 +0200

    safe-guarded the routes with an auth middleware

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,18 @@
+const requireCurrentUser = require('../middlewares/requireCurrentUser');
 const authRouter = require('./auth');
 const currentUserRouter = require('./currentUser');
 const locationsRouter = require('./locations');
 const storageRouter = require('./storage');
+const syncRouter = require('./sync');
 
 module.exports = (app) => {
   app.use('/auth', authRouter);
-  app.use('/currentUser', currentUserRouter);
-  // app.use('/sensors', sensorsRouter);
-  // app.use('/experiments', experimentsRouter);
-  app.use('/locations', locationsRouter);
+  app.use('/currentUser', requireCurrentUser, currentUserRouter);
+  app.use('/sync', requireCurrentUser, syncRouter);
+  app.use('/locations', requireCurrentUser, locationsRouter);
   app.use(
     '/locations/:locationId/experiments/:experimentId/storage',
+    requireCurrentUser,
     storageRouter
   );
 };
