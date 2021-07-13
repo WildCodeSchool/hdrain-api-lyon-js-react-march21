8aa6880fc19c24d614815ee41611a313cee0fecd
Edouard Misset <edouardmisset@gmail.com>
2021-07-13 16:23:14 +0200

    removed durable option in rabbit to be done with the previous messages

--- rabbitWorker.js
+++ rabbitWorker.js
@@ -2,7 +2,6 @@
 const amqp = require('amqplib/callback_api');
 const storeData = require('./storeRabbitData');
 
-
 function connexion() {
   // connect to RabbitMQ server
   amqp.connect(
@@ -17,20 +16,16 @@ function connexion() {
         if (error1) {
           throw error1;
         }
-
         // To send, we must declare a queue for us to send to; then we can publish a message to the queue
         const queue = 'HDR_Monitoring';
-
         // This makes sure the queue is declared before attempting to consume from it
         channel.assertQueue(queue, {
-          durable: true,
+          // durable: true,
         });
-
         console.log(
           ' [*] Rabbit - Waiting for messages in %s. Press CTRL+C to exit',
           queue
         );
-
         channel.consume(
           queue,
           async (msg) => {
@@ -38,11 +33,9 @@ function connexion() {
             try {
               const rabbitMqData = JSON.parse(message);
               storeData(rabbitMqData);
-
             } catch (error) {
               console.error(error);
             }
-            
           },
           {
             // automatic acknowledgment mode,
