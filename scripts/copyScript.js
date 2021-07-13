2295ddc0190538d2481dd46e5f28adfffd9ce6f5
Edouard Misset <edouardmisset@gmail.com>
2021-07-13 16:25:08 +0200

    removed the interval

--- scripts/copyScript.js
+++ scripts/copyScript.js
@@ -9,38 +9,18 @@ const source = process.env.HD_RAIN_SOURCE;
 
 // Returns a shell command to copy the file server's content to our server (using rsync through ssh)
 const command = (path, address) =>
-  ` rsync -vrzh --rsh='sshpass -p ${process.env.HD_RAIN_SERVER_PWD} ssh -o "StrictHostKeyChecking no"' wild@${address}:${source} ${target}/${path}`;
-
-// Interval in milliseconds
-const interval = 5 * 60 * 1000;
-let intervalId = null;
+  `rsync -vrzh --rsh='sshpass -p ${process.env.HD_RAIN_SERVER_PWD} ssh -o "StrictHostKeyChecking no"' wild@${address}:${source} ${target}/${path}`;
 
 // Function to execute the shell command to copy the data to our server
-const copyData = (cmd) => {
-  console.log(new Date());
-  shell.exec(cmd);
+const copyData = async () => {
+  console.log('Copy started at ', new Date());
+  return Promise.all(
+    destinationPathList.map((path, index) =>
+      shell.exec(command(path, serverAddressList[index]), { async: true })
+    )
+  );
 };
 
-// Function to start the periodic copy of files every 5 minutes
-const copyDataPeriodically = () => {
-  intervalId = setInterval(() => {
-    destinationPathList.forEach((path, index) => {
-      copyData(command(path, serverAddressList[index]));
-    });
-  }, interval);
-  console.log(`Interval was started with ID: ${intervalId}`);
-};
-
-// Function to stop the periodic copy of files
-const stopCopyingData = () => {
-  clearInterval(intervalId);
-  console.log(`Interval ${intervalId} was stopped`);
-};
-
-module.exports = {
-  copyDataPeriodically,
-  copyData,
-  stopCopyingData,
-};
+module.exports = copyData;
 
 // To install sshpass on Linux: apt-get install sshpass
