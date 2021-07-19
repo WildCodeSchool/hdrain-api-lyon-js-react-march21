683639953985d5239df9629ab1eb83466bef1ee1
Heranca <heranca.kamalo@gmail.com>
2021-07-19 19:25:26 +0200

    test ok

--- storeRabbitData.js
+++ storeRabbitData.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const ReadableData = require('stream').Readable;
+const { promises: fsp } = require('fs');
+const { join } = require('path');
 const ExperimentModel = require('./models/ExperimentModel');
 const StatusModel = require('./models/StatusModel');
 const SensorModel = require('./models/SensorModel');
@@ -10,20 +10,18 @@ const LocationModel = require('./models/LocationModel');
 // helper 1 : restructuring the data to made it suitable for the db
 const cleanData = (data) => {
   // data to send in the database
-  if (!data.config) {
+  if (!data.Config) {
     return null;
   }
 
   const experimentData = {
     timestamp: data.date.replace(/h/, '_').split('_'),
-    assimilationLog: data['log assim'],
-    neuralNetworkLog: data['log rn'],
-    parameters: data.config,
-    status: JSON.parse(data.statuts),
+    assimilationLog: data['LOG assim'],
+    neuralNetworkLog: data['LOG RN'],
+    parameters: data.Config,
+    status: JSON.parse(data.Statuts),
     sensorsPosition: JSON.parse(JSON.parse(data.geometrie)),
-    location: data.location
-      ? data.location.charAt(0).toUpperCase() + data.location.slice(1)
-      : 'Abidjan',
+    location: data.location ? data.location : 'Abidjan',
     locCoord: {
       lat: data.coordoonees ? data.coordoonees[0] : 'undefined',
       lng: data.coordoonees ? data.coordoonees[1] : 'undefined',
@@ -31,12 +29,8 @@ const cleanData = (data) => {
     sendingDate: data['date envoi']
       ? data['date envoi'].replace(/-,:/g, '_').split('_')
       : 'undefined',
-    rainMap: data['champs assim']
-      ? data['champs assim']
-      : 'undefined',
-    costGraph: data.diagnostics
-      ? data.diagnostics
-      : 'undefined',
+    rainMap: data['champs assim'] ? data['champs assim'] : 'undefined',
+    costGraph: data.diagnostics ? data.diagnostics : 'undefined',
   };
 
   // change date fortmat
@@ -63,44 +57,73 @@ const cleanData = (data) => {
 
 // helper 2.1 : check if the experiment already exist in the db
 
-const checkDbForExperiment = async (experimentToCheck) => {
-  await ExperimentModel.getExperiment(experimentToCheck);
+// const checkDbForExperiment = async (experimentToCheck) => {
+
+// };
+
+// helper 2.2 : store image in server and get the path
+
+const storeImgInServer = async (expData) => {
+  if (expData.rainMap === 'undefined') return expData;
+
+  const rainMapbase64 = expData.rainMap;
+  const costGraphbase64 = expData.costGraph;
+
+  const rainMapBufferData = Buffer.from(rainMapbase64, 'base64');
+  const costGraphBufferData = Buffer.from(costGraphbase64, 'base64');
+
+  // date format
+  const expYear = expData.timestamp.getFullYear();
+  const expMonth = expData.timestamp.getMonth();
+  const expDay = expData.timestamp.getDate();
+  const expHours = expData.timestamp.getHours();
+  const expMinutes = expData.timestamp.getMinutes();
+
+  const date = `${expYear}_${expMonth}_${expDay}_${expHours}h${expMinutes}`;
+
+  const fileNameRainMap = `rainMap_${date}.png`;
+  const fileNameCostGraph = `costGraph_${date}.png`;
+
+  const rainMapPath = join('storage', 'images', fileNameRainMap);
+  const costGraphPath = join(
+    'storage',
+    'images',
+    fileNameCostGraph
+  );
+
+  // store rain map
+  await fsp.writeFile(rainMapPath, rainMapBufferData);
+
+  // store cost graph
+  await fsp.writeFile(costGraphPath, costGraphBufferData);
+  console.log('images stored');
+
+  // re-assign path to the exp object
+  const DataWithPath = expData;
+
+  DataWithPath.rainMap = rainMapPath;
+  DataWithPath.costGraph = costGraphPath;
+
+  return DataWithPath;
 };
-
-// helper 2.2 : store image in server and get the path 
-
-const storeImgInServer = (expData) => {
-  var rainMapBase64 = expData.rainGraph;
-  var costGraphBase64 = expData.costGraph;
-
-  const imageBufferData = Buffer.from(base64, ‘base64’)
-  var streamObj = new ReadableData()
-  streamObj.push(imageBufferData)
-  streamObj.push(null)
-  streamObj.pipe(fs.createWriteStream(‘testImage.jpg’));
-
-};
-
 
 // helper 3 store expriment
 const saveExperiment = async (experiment) => {
   // check if the experiment already
-  const experimentExists = await checkDbForExperiment(experiment);
+  const experimentExists = await ExperimentModel.getExperiment(experiment);
 
   if (!experimentExists) {
-    const data = experiment;
-
-    // store rainMap and cost graph 
-
+    // store rainMap and cost graph
+    const ExpWithImgPath = await storeImgInServer(experiment);
 
     // new experiment storing
-    const storedExperiment = await ExperimentModel.create(data);
+    const storedExperiment = await ExperimentModel.create(ExpWithImgPath);
 
     console.log('experiment stored in DB: ', storedExperiment.id);
 
-    data.experimentId = storedExperiment.id;
+    ExpWithImgPath.experimentId = storedExperiment.id;
 
-    return data;
+    return ExpWithImgPath;
   }
   return undefined;
 };
@@ -169,12 +192,10 @@ const storeStatus = async (listOfSensors, newExperimentData) => {
 
 // helper 7 : check location
 
-
 const changeLocationId = async (arrayOfLocations, expSaved) => {
   const [expWithLocation] = arrayOfLocations.filter(
     (location) => location.name === expSaved.location
   );
-
 
   return { ...expSaved, locationId: expWithLocation.id };
 };
